89609654d372d42cf31bcdbc0327a2155bbaa483
azu <azuciao@gmail.com>
2013-11-10 13:41:59 +0900

    init

---
+++ GM_setValue_with_big_value.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @id             GM_setValue_with_big_value
+// @name           GM_setValue_with_big_value
+// @version        1.0
+// @namespace      http://efcl.info/
+// @author         azu
+// @description    
+// @include        http*
+// @resource       1kb  https://rawgithub.com/azu/GM_setValue_with_big_value/master/1kb.dummy
+// @run-at         document-end
+// ==/UserScript==
