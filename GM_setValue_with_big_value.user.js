33df4dc18348a2c6b2fdf5f2f4d5137cbc5fd7b2
azu <azuciao@gmail.com>
2013-11-10 13:52:01 +0900

    update

--- GM_setValue_with_big_value.user.js
+++ GM_setValue_with_big_value.user.js
@@ -9,3 +9,16 @@
 // @resource       1kb  https://rawgithub.com/azu/GM_setValue_with_big_value/master/1kb.dummy
 // @run-at         document-end
 // ==/UserScript==
+
+var kb_one = GM_getResourceText("1kb");
+
+var data = "";
+for (var i = 0; i < 10; i++) {
+    data = data + kb_one;
+    try {
+        GM_setValue("test", kb_one);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
